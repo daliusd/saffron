0e0c1fa3048d1e7534c89c6b42c826ec16113968
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2019-02-12 21:53:40 +0200

    Redirect to login.

--- src/CardSetPage.tsx
+++ src/CardSetPage.tsx
@@ -1,23 +1,34 @@
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
 
 import { Dispatch, cardSetSelectRequest } from './actions';
+import { State } from './reducers';
 import CardSet from './components/CardSet';
 
 interface Props {
     dispatch: Dispatch;
     match: { params: { id: string } };
+    isAuthenticated: boolean;
 }
 
 export class CardSetPage extends Component<Props> {
     componentDidMount() {
-        const { dispatch, match } = this.props;
-        dispatch(cardSetSelectRequest(match.params.id));
+        const { dispatch, match, isAuthenticated } = this.props;
+        if (isAuthenticated) {
+            dispatch(cardSetSelectRequest(match.params.id));
+        }
     }
 
     render() {
-        return <CardSet />;
+        if (this.props.isAuthenticated) {
+            return <CardSet />;
+        } else {
+            return <Redirect to="/login" />;
+        }
     }
 }
 
-export default connect()(CardSetPage);
+const mapStateToProps = (state: State) => ({ isAuthenticated: state.auth.isAuthenticated });
+
+export default connect(mapStateToProps)(CardSetPage);
--- src/GamePage.tsx
+++ src/GamePage.tsx
@@ -1,23 +1,34 @@
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
 
 import { Dispatch, gameSelectRequest } from './actions';
+import { State } from './reducers';
 import CardSets from './components/CardSets';
 
 interface Props {
     dispatch: Dispatch;
     match: { params: { id: string } };
+    isAuthenticated: boolean;
 }
 
 export class GamePage extends Component<Props> {
     componentDidMount() {
-        const { dispatch, match } = this.props;
-        dispatch(gameSelectRequest(match.params.id, true));
+        const { dispatch, match, isAuthenticated } = this.props;
+        if (isAuthenticated) {
+            dispatch(gameSelectRequest(match.params.id, true));
+        }
     }
 
     render() {
-        return <CardSets />;
+        if (this.props.isAuthenticated) {
+            return <CardSets />;
+        } else {
+            return <Redirect to="/login" />;
+        }
     }
 }
 
-export default connect()(GamePage);
+const mapStateToProps = (state: State) => ({ isAuthenticated: state.auth.isAuthenticated });
+
+export default connect(mapStateToProps)(GamePage);
