194bfa679ab709bae05ae7edaebc3925061b624a
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2019-02-17 13:45:36 +0200

    Error boundry added.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { State } from './reducers';
 import AboutPage from './AboutPage';
 import CardSetPage from './CardSetPage';
 import ChangelogPage from './ChangelogPage';
+import ErrorBoundry from './ErrorBoundry';
 import GamePage from './GamePage';
 import LoginPage from './LoginPage';
 import Main from './Main';
@@ -31,18 +32,20 @@ export class App extends Component<Props> {
         return (
             <Router>
                 <div className="App">
-                    <Navbar isAuthenticated={isAuthenticated} />
-                    <Switch>
-                        <Route exact path="/" component={Main} />
-                        <Route exact path="/login" component={LoginPage} />
-                        <Route exact path="/signup" component={SignUpPage} />
-                        <Route exact path="/game/:id" component={GamePage} />
-                        <Route exact path="/cardset/:id" component={CardSetPage} />
-                        <Route exact path="/changelog" component={ChangelogPage} />
-                        <Route exact path="/about" component={AboutPage} />
-                        <Route component={NotFoundPage} />
-                    </Switch>
-                    <div className="App-footer">Dalius Dobravolskas &copy; {new Date().getFullYear()}</div>
+                    <ErrorBoundry>
+                        <Navbar isAuthenticated={isAuthenticated} />
+                        <Switch>
+                            <Route exact path="/" component={Main} />
+                            <Route exact path="/login" component={LoginPage} />
+                            <Route exact path="/signup" component={SignUpPage} />
+                            <Route exact path="/game/:id" component={GamePage} />
+                            <Route exact path="/cardset/:id" component={CardSetPage} />
+                            <Route exact path="/changelog" component={ChangelogPage} />
+                            <Route exact path="/about" component={AboutPage} />
+                            <Route component={NotFoundPage} />
+                        </Switch>
+                        <div className="App-footer">Dalius Dobravolskas &copy; {new Date().getFullYear()}</div>
+                    </ErrorBoundry>
                 </div>
             </Router>
         );
---
+++ src/ErrorBoundry.tsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import KawaiiMessage, { Character } from './components/KawaiiMessage';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+interface State {
+    hasError: boolean;
+}
+
+export default class ErrorBoundary extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    componentDidCatch() {
+        this.setState({ hasError: true });
+        // You can also log the error to an error reporting service
+        // logErrorToMyService(error, info);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <KawaiiMessage character={Character.Browser} mood="xo">
+                    Something unexpected has happened. If you want this problem to be fixed write me to{' '}
+                    <a href="mailto:dalius@ffff.lt">dalius@ffff.lt</a>.
+                </KawaiiMessage>
+            );
+        }
+        return this.props.children;
+    }
+}
